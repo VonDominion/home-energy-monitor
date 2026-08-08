fd6533303babe32faeee1bc8b38117f42b320e3a
Mayank Chhipa <lovekrishna10000@gmail.com>
2026-08-09 00:50:05 +0530

    Add Pages and Components Folder

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,34 +1,37 @@
 import { Routes, Route } from "react-router-dom"
+import PublicLayout from "../Layouts/PublicLayout/PublicLayout"
+import DashboardLayout from "../Layouts/DasboardLayout/DasboardLayout"
 
 function AppRoutes() {
   return (
     <>
-        {/* Public Routes*/}
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} /> {/* Need to be replaced by <Home /> */}
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/register" element={<h1>Register</h1>} />
-      </Routes>
+
+        {/* Public Routes*/}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<h1>Home</h1>} /> {/* Need to be replaced by <Home /> */}
+          <Route path="/login" element={<h1>Login</h1>} />
+          <Route path="/register" element={<h1>Register</h1>} />
+        </Route>
 
         {/* Protected Routes */}
 
-      <Routes path="/dashboard">
-        <Route index element={<h1>Dashboard</h1>} />
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<h1>Dashboard</h1>} />
 
-        <Route
-          path="appliances"
-          element={<h1>Appliances</h1>}
-        />
+          <Route
+            path="appliances"
+            element={<h1>Appliances</h1>}
+          />
 
-        <Route
-          path="profile"
-          element={<h1>Profile</h1>}
-        />
+          <Route
+            path="profile"
+            element={<h1>Profile</h1>}
+          />
 
+        </Route>
       </Routes>
     </>
-
-
 
   )
 }
