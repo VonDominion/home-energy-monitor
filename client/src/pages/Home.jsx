4589763374dc9fdd2eed57507cde41a5a26be4be
Mayank Chhipa <lovekrishna10000@gmail.com>
2026-08-23 23:59:58 +0530

    Added Public Navbar and Footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import HeroSection from "../components/home/HeroSection";
+import WhyUsSection from "../components/home/WhyUsSection";
+import HowItWorksSection from "../components/home/HowItWorksSection";
+import CTASection from "../components/home/CTASection";
 
 function Home() {
   return (
 
     <>
       <HeroSection></HeroSection>
+      <WhyUsSection></WhyUsSection>
+      <HowItWorksSection></HowItWorksSection>
+      <CTASection></CTASection>
     </>
 
   );
