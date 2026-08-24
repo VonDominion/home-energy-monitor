1162505ea52c50b022b74bbc858bcf378418b93c
Naitik-Tiwari <krishnacod419@gmail.com>
2026-08-24 10:58:39 +0530

    faet: optimization

--- server/src/routes/health.routes.js
+++ server/src/routes/health.routes.js
@@ -3,8 +3,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/health', (req, res)=> {
-    res.json({
-        status: 'Home',
+    res.status(200).json({
+        status: 'ok',
         message: 'Home Energy Monitor API is Running'
     });
 });
