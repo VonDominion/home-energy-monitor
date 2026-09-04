c56d59e8d21f6d74155e5143fc895400329c872e
Mayank Chhipa <lovekrishna10000@gmail.com>
2026-09-04 22:05:08 +0530

    edit

---
+++ server/src/routes/auth.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const {
+    signup,
+    login,
+    getMe
+} = require('../controllers/auth.controller');
+
+const authMiddleware = require('../middleware/auth.middleware');
+
+const router = express.Router();
+
+router.post('/signup', signup);
+router.post('/login', login);
+router.get('/me', authMiddleware, getMe);
+
+module.exports = router;
