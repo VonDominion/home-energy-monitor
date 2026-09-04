c8d9fd63f9191af9c1e16cb5be6c2fea7d857c25
Mayank Chhipa <lovekrishna10000@gmail.com>
2026-09-04 20:08:56 +0530

    Login and Register Backend Integration.

---
+++ client/src/routes/AppRoutes.jsx
@@ -0,0 +1,51 @@
+import { Routes, Route } from "react-router-dom"
+import PublicLayout from "../Layouts/PublicLayout/PublicLayout"
+import DashboardLayout from "../Layouts/DasboardLayout/DasboardLayout"
+
+import Home from "../pages/Home.jsx"
+import Login from "../pages/Login.jsx"
+import Register from "../pages/Register.jsx"
+import Appliances from "../pages/Appliances.jsx"
+import Dashboard from "../pages/Dashboard.jsx"
+import Profile from "../pages/Profile.jsx"
+import ForgotPassword from "../pages/ForgotPassword.jsx"
+
+function AppRoutes() {
+  return (
+    <>
+      <Routes>
+
+        {/* Public Routes*/}
+        <Route element={<PublicLayout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route 
+            path="/forgot-password" 
+            element={<ForgotPassword />} 
+          />
+        </Route>
+
+        {/* Protected Routes */}
+
+        <Route path="/dashboard" element={<DashboardLayout />}>
+          <Route index element={<Dashboard />} />
+
+          <Route
+            path="appliances"
+            element={<Appliances />}
+          />
+
+          <Route
+            path="profile"
+            element={<Profile />}
+          />
+
+        </Route>
+      </Routes>
+    </>
+
+  )
+}
+
+export default AppRoutes
